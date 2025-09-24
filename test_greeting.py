41f1a201b6a2237160254e5f2ea4ef8df035ce79
Minato2404 <billykarma24@gmail.com>
2025-09-24 16:02:31 +0700

    Fix import error pada test_greeting.py

--- test_greeting.py
+++ test_greeting.py
@@ -1,4 +1,9 @@
+import sys
+import os
+sys.path.append(os.path.dirname(__file__))
+
 from greeting import greeting
 
 def test_greeting():
     assert greeting("Polibest") == "Hello Polibest, welcome to DevSecOps!"
+
